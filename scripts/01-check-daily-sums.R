78dda87ec5a0bbf3bfdf820ec166a19f323d9348
Jessica Guo <jessicaguo@email.arizona.edu>
2023-06-27 12:31:39 -0700

    Run on cleaned data and write out daily sums

--- scripts/01-check-daily-sums.R
+++ scripts/01-check-daily-sums.R
@@ -2,7 +2,7 @@
 library(tidyverse)
 library(janitor)
 
-dat <- read_csv("data_raw/saguaro_flowering_2014.csv") %>%
+dat <- read_csv("data_raw/saguaro_data_2014.csv") %>%
   janitor::clean_names() %>%
   mutate(date = mdy(date))
 
@@ -13,3 +13,6 @@ length(unique(dat$date))
 daily_total <- dat %>%
   group_by(date) %>%
   summarize(total_blooms = sum(number_blooms))
+
+
+write_csv(daily_total, "data_raw/daily_sums_2014.csv")
