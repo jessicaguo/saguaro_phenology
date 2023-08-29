3b69900af0663787aa0378bd55f095a83a168760
Jessica Guo <jessicaguo@email.arizona.edu>
2023-08-29 11:13:22 -0700

    Add 2015 data + plot

--- scripts/01-check-daily-sums.R
+++ scripts/01-check-daily-sums.R
@@ -2,7 +2,7 @@
 library(tidyverse)
 library(janitor)
 
-dat <- read_csv("data_raw/saguaro_data_2014.csv") %>%
+dat <- read_csv("data_raw/saguaro_data_2015.csv") %>%
   janitor::clean_names() %>%
   mutate(date = mdy(date))
 
@@ -15,4 +15,18 @@ daily_total <- dat %>%
   summarize(total_blooms = sum(number_blooms))
 
 
-write_csv(daily_total, "data_raw/daily_sums_2014.csv")
+write_csv(daily_total, "data_raw/daily_sums_2015.csv")
+
+### Combine datasets so far
+ds14 <- read_csv("data_raw/daily_sums_2014.csv")
+ds15 <- read_csv("data_raw/daily_sums_2015.csv")
+
+comb <- ds14 |> 
+  bind_rows(ds15)
+
+comb |> 
+  mutate(year = lubridate::year(date)) |> 
+ggplot(aes(x = date, y = total_blooms)) + 
+  geom_point() +
+  facet_wrap(~year, scales = "free",
+             ncol = 1)
